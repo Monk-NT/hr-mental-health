22f7216c14b5aa4259d2802c7881a3a983a92d61
monk.nt@gmail.com <monk.nt@gmail.com@162cf27c-bc33-e75a-f599-8e1340f72a5b>
2010-12-05 18:29:59 +0000

    Added punctuation symbols calculation

--- trunk/PerlScripts/wordCount.pl
+++ trunk/PerlScripts/wordCount.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
 
+%intpunct;
 while(<STDIN>)
 {
 	$line = $_;
@@ -7,13 +8,77 @@ while(<STDIN>)
 	@words=split(/ /,$line);
 	foreach $word (@words)
 	{
-		
+		#if word is a word
 		if($word=~m/^?[a-zA-Z]+?.$/)
 		{
+			
+				if($word=~m/.*\./)
+				{
+					$intpunct{"."}++;
+					
+					
+				}
+				if($word=~m/.!/)
+				{
+					
+					$intpunct{"!"}++;
+					
+				}
+				if($word=~m/.\?/)
+				{
+				
+					$intpunct{"?"}++;
+					
+				}
+				if($word=~m/.!/)
+				{
+					
+					$intpunct{"!"}++;
+					
+				}
+				if($word=~m/.,/)
+				{
+					
+					$intpunct{","}++;
+				
+				}
+				if($word=~m/.;/)
+				{
+					$intpunct{";"}++;
+					
+				}
+				if($word=~m/.:/)
+				{
+					
+					$intpunct{":"}++;
+					
+				}
+				if($word=~m/.-/)
+				{
+					
+					$intpunct{"-"}++;
+					
+				}
+			
 			$i++;
 			print "$word";
 			print "\n";
 		}
 	}
+	
 }
 print "$i";	
+print "INTERPUNCION:\n";
+my $tintpct;
+for my $vals (values %intpunct)
+{
+	$tintpct=$tintpct+$vals;
+}
+
+for my $key ( keys %intpunct)
+{
+	$intpunct{$key}/=$tintpct;
+	my $value = $intpunct{$key};
+	print "$key->";
+print"	$value\n";
+}
