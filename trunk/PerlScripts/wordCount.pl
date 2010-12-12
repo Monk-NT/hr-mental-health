bf692a43653fc574166d6a665777d4887041f25a
monk.nt@gmail.com <monk.nt@gmail.com@162cf27c-bc33-e75a-f599-8e1340f72a5b>
2010-12-12 11:25:28 +0000

    Some formatting in wordCount.pl, added subroutines wordLen, functionW, punctmarks, and printAll, added docs/tehnicka-dokumentacija.txt

--- trunk/PerlScripts/wordCount.pl
+++ trunk/PerlScripts/wordCount.pl
@@ -3,6 +3,7 @@
 %intpunct;
 %wlength;
 %fwords;
+
 open FILE, "data/functionWords.txt" or die $!;
 while(<FILE>)
 {	chomp($_);
@@ -10,119 +11,103 @@ while(<FILE>)
 }
 close FILE;
 open FILE, ">>data.arff" or die $!;
-while(<STDIN>)
-{
+while(<STDIN>){
+
 	$line = $_;                          
 	chomp($line);
 	@words=split(/ /,$line);
-	foreach $word (@words)
-	{
+	
+	foreach $word (@words){
+		
 		#if word is a word, bird is the word!
-		if($word=~m/^?[a-zA-Z]+?.$/)
-		{
-				#check for punctionation marks
-				if($word=~m/.*\./)
-				{
-					$intpunct{"."}++;
-					
-					
-				}
-				if($word=~m/.!/)
-				{
-					
-					$intpunct{"!"}++;
-					
-				}
-				if($word=~m/.\?/)
-				{
-				
-					$intpunct{"?"}++;
-					
-				}
-				if($word=~m/.,/)
-				{
-					
-					$intpunct{","}++;
-				
-				}
-				if($word=~m/.;/)
-				{
-					$intpunct{";"}++;
-					
-				}
-				if($word=~m/.:/)
-				{
-					
-					$intpunct{":"}++;
-					
-				}
-				if($word=~m/.-/)
-				{
-					
-					$intpunct{"-"}++;
-					
-				}
+		if($word=~m/^?[a-zA-Z]+?.$/){
 			
-			$i++;
+			#check for punctionation marks
+			punctmarks($word);
+			
 			#word length
-			if(length($word)>=10)
-			{
-				$wlength{10}++;
-				print "10+ ";
-			}
-			else
-			{
-				$wlength{length($word)}++;
-				print " ";
-				print length($word);
-			}
-			for my $key (keys %fwords)
-			{
-				if(lc($key) eq lc($word))
-				{
-					$fwords{$key}++;
-				}
-			}
+			wordLen($word);
+			
+			#function words
+			functionW($words);
+			
 			#word total
 			$wTotal++;
 		}
 	}
-	
 }
 
-for my $key (keys %fwords)
+printAll();
+
+#subroutine checks if punctiation marks exist in the word.
+sub punctmarks
 {
-	$fwords{$key}/=$wTotal;
+	if($_[0]=~m/.*\./){
+		$intpunct{"."}++;				
+	}
+	if($_[0]=~m/.!/){
+		$intpunct{"!"}++;
+	}
+	if($_[0]=~m/.\?/){
+		$intpunct{"?"}++;
+	}
+	if($_[0]=~m/.,/){
+		$intpunct{","}++;
+	}
+	if($_[0]=~m/.;/){
+		$intpunct{";"}++;
+	}
+	if($_[0]=~m/.:/){
+		$intpunct{":"}++;
+	}
+	if($_[0]=~m/.-/){
+		$intpunct{"-"}++;
+	}
 }
-
-print FILE "\n";
-
-
-my $tintpct;
-for my $key (keys %wlength)
+#subroutine counts words with the same length
+sub wordLen
 {
-	$wlength{$key}/=$wTotal;
-	
-		if(defined($wlength{$key}))
-			{print FILE "$wlength{$key},";}
-		else
-			{print FILE "0,";}
-	
+	if(length($_[0])>=10){
+		$wlength{10}++;
+	}
+	else{
+		$wlength{length($_[0])}++;
+	}
 }
-for my $vals (values %intpunct)
+#subroutine counts function words
+sub functionW
 {
-	$tintpct=$tintpct+$vals;
+	for my $key (keys %fwords){
+		if(lc($key) eq lc($_[0])){
+			$fwords{$key}++;
+		}
+	}
 }
-
-for my $key ( keys %intpunct)
+#subroutine does final calculations and prints output
+sub printAll
 {
-	$intpunct{$key}/=$tintpct;
-	my $value = $intpunct{$key};
-	print FILE "$value,";
+	my $tintpct;
+	print FILE "\n";
+	for my $key (keys %wlength){
+		$wlength{$key}/=$wTotal;
+		
+		if(defined($wlength{$key})){
+			print FILE "$wlength{$key},";
+		}
+		else{
+			print FILE "0,";
+		}
+	}
+	for my $vals (values %intpunct){
+		$tintpct=$tintpct+$vals;
+	}
 
-	
-}
-for my $key (keys %fwords)
-{
-	print FILE "$fwords{$key},";
+	for my $key ( keys %intpunct){
+		$intpunct{$key}/=$tintpct;
+		print FILE "$intpunct{$key},";
+	}
+	for my $key (keys %fwords){
+		$fwords{$key}/=$wTotal;
+		print FILE "$fwords{$key},";
+	}
 }
